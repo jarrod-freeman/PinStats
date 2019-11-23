7db009df5c4225b549a2d3a85d2030b491ad6c54
jarrod-freeman <51888879+jarrod-freeman@users.noreply.github.com>
2019-11-23 15:08:57 -0500

    convert TournamentDetailsComponent to a functional component

--- src/components/ifpa/tournament/TournamentDetailsComponent.tsx
+++ src/components/ifpa/tournament/TournamentDetailsComponent.tsx
@@ -1,67 +1,57 @@
-import React, {Component, Props} from 'react';
+import React, {FunctionComponent, Props, useState, useEffect} from 'react';
 import {RouteComponentProps} from 'react-router-dom';
 import Tournament from '../../../models/ifpa/Tournament';
 import  '../../../css/TournamentList.css';
 import tournamentService from '../../../services/tournaments';
 
-type RouteParams = {
-      TournamentID: string,
-      EventName: string
-  }
+interface RouteParams {
+    TournamentID: string,
+    EventName: string
+};
 
-type MyState = { tournament: Tournament };
+const TournamentDetailsComponent: FunctionComponent<any> = (props: RouteParams) => {
+    const [tournament, setTournament] = useState(new Tournament({}));
 
-class TournamentDetailsComponent extends Component<RouteParams, MyState>{
-    constructor(props: RouteParams){
-        super(props);
-        
-        this.state = {
-            tournament: new Tournament({})
-        }
-    }
-
-    public componentDidMount(){
-        tournamentService.getTournament(this.props.TournamentID, this.props.EventName)
+    useEffect(() => {
+        tournamentService.getTournament(props.TournamentID, props.EventName)
             .then(tournament => {
                 if(tournament instanceof Tournament){
-                    this.setState({tournament: tournament});
+                    setTournament(tournament);
                 }
                 else{
-                    this.setState({tournament: new Tournament({})});
+                    setTournament(new Tournament({}));
                 }
             });
-    }
+    }, []);
     
-    public getEventWinner(){
-        if(this.state.tournament.Events && this.state.tournament.Events.length > 0){
-            return this.state.tournament.Events[0].WinnerName;
+    const getEventWinner = () => {
+        if(tournament.Events && tournament.Events.length > 0){
+            return tournament.Events[0].WinnerName;
         }
 
         return null;
     }
 
-    public getEventDate(){
-        if(this.state.tournament.Events && this.state.tournament.Events.length > 0){
-            return this.state.tournament.Events[0].Date.toLocaleDateString();
+    const getEventDate = () => {
+        if(tournament.Events && tournament.Events.length > 0){
+            return tournament.Events[0].Date.toLocaleDateString();
         }
 
         return null;
     }
 
-    public render(){
-        return (
+    return (
+        <div>
+            <h2>{tournament.Name} - {props.EventName}</h2>
+
             <div>
-                <h2>{this.state.tournament.Name} - {this.props.EventName}</h2>
-
-                <div>
-                    Winner: {this.getEventWinner()}
-                </div>
-                <div>
-                    Date: {this.getEventDate()}
-                </div>
+                Winner: {getEventWinner()}
             </div>
-        );
-    }
+            <div>
+                Date: {getEventDate()}
+            </div>
+        </div>
+    );
 }
 
 export interface TournamentDetailsProps extends RouteComponentProps<RouteParams>, Props<RouteParams> {}
