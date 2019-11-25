a30a57dfa1bcffba66b4cc1d131127ca61b0163c
jarrod-freeman <51888879+jarrod-freeman@users.noreply.github.com>
2019-11-24 21:00:42 -0500

    start of responsive design

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -8,20 +8,29 @@ import Profile from '../ifpa/ProfileComponent';
 import TournamentList from '../ifpa/tournament/TournamentList';
 import TournamentDetailsComponent, { TournamentDetailsProps } from '../ifpa/tournament/TournamentDetailsComponent';
 import HomeComponent from '../HomeComponent';
+import Grid from '@material-ui/core/Grid';
+import Container from '@material-ui/core/Container';
 
 const App = () => {
     return (
         <div className="App">
             <HashRouter>
-                <Header />
-                <NavMenu />
-
-                <div className="content">
-                    <Route exact path="/" component={HomeComponent} />
-                    <Route exact path="/tournaments" component={TournamentList} />
-                    <Route path="/tournaments/:TournamentID/:EventName" render={ (props: TournamentDetailsProps) => { return <TournamentDetailsComponent TournamentID={props.match.params.TournamentID}  EventName={props.match.params.EventName} />; } } />
-                    <Route path="/players" component={Profile} />
-                </div>
+                <Grid container spacing={0} justify="center">
+                    <Grid item xs={12} md={9}>
+                        <Header />
+                    </Grid>
+                    <Grid item xs={12} md={9}>
+                        <NavMenu />
+                    </Grid>
+                    <Grid className="content" item xs={12} md={9}>
+                        <Container >
+                            <Route exact path="/" component={HomeComponent} />
+                            <Route exact path="/tournaments" component={TournamentList} />
+                            <Route path="/tournaments/:TournamentID/:EventName" render={ (props: TournamentDetailsProps) => { return <TournamentDetailsComponent TournamentID={props.match.params.TournamentID}  EventName={props.match.params.EventName} />; } } />
+                            <Route path="/players" component={Profile} />
+                        </Container>
+                    </Grid>
+                </Grid>
             </HashRouter>
             <Footer />
         </div>
