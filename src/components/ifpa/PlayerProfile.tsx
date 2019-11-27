f39a1741a0a4f719913427e7dc4b48cb82ed576d
jarrod-freeman <51888879+jarrod-freeman@users.noreply.github.com>
2019-11-27 18:22:31 -0500

    Add alt text to player profile image

--- src/components/ifpa/PlayerProfile.tsx
+++ src/components/ifpa/PlayerProfile.tsx
@@ -11,7 +11,7 @@ const PlayerProfile: FunctionComponent<PlayerProfileParams> = ({ Player }: Playe
         <section>
             <h3>Player Info</h3>
             <Grid container spacing={2}>
-                <Grid item xs={12} sm={2}><img style={{ width: '100%' }} src={`https://www.ifpapinball.com/images/profiles/players/${Player.ID}.jpg`} /></Grid>
+                <Grid item xs={12} sm={2}><img style={{ width: '100%' }} src={`https://www.ifpapinball.com/images/profiles/players/${Player.ID}.jpg`} alt="player avatar" /></Grid>
                 <Grid item xs={12} sm={10}>
                     <Grid container spacing={2}>
                         <Grid item xs={12} sm={2}>Name:</Grid>
