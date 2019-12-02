d4146c79d9561f557d1806e2b4f987851160eea6
jarrod-freeman <51888879+jarrod-freeman@users.noreply.github.com>
2019-12-01 20:41:42 -0500

    Fix useHistory unit testing issue

--- src/tests/components/TournamentPage.test.tsx
+++ src/tests/components/TournamentPage.test.tsx
@@ -2,8 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import TournamentPage from '../../components/pages/TournamentPage';
 
-it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<TournamentPage />, div);
-    ReactDOM.unmountComponentAtNode(div);
+jest.mock('react-router-dom', () => ({
+    useHistory: () => ({
+        push: jest.fn(),
+        listen: jest.fn().mockImplementation(() => jest.fn())
+    }),
+}));
+
+describe('TournamentPage Component', () => {
+    test('renders without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<TournamentPage />, div);
+        ReactDOM.unmountComponentAtNode(div);
+    });
 });
